b47ced3ace823cecab7de6696e94763f6a0ce9b1
negiinj <neginjahan10@gmail.com>
2024-02-09 15:58:08 +0100

    NotificationsFragment.java updated to merge

--- app/src/main/java/com/example/hydroponic_farm/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/hydroponic_farm/ui/notifications/NotificationsFragment.java
@@ -18,15 +18,32 @@ public class NotificationsFragment extends Fragment {
 
     private FragmentNotificationsBinding binding;
     AlarmsDataset dataset;
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
 
-        binding = FragmentNotificationsBinding.inflate(inflater, container, false);
-        View root = binding.getRoot();
-        SharedPreferences sp=requireContext().getSharedPreferences("DIRECTORY1", Context.MODE_PRIVATE);
-        dataset=new AlarmsDataset(NotificationsFragment.this.getContext(),sp.getString("token", ""));
-        dataset.fill();//Should fill in a different thread?
-        return root;
+    private ListView alarmsListView;
+    private MyAdapter alarmAdapter;
+    private ArrayList<Alarm> alarmList = new ArrayList<>();
+
+    public NotificationsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        return inflater.inflate(R.layout.fragment_notifications, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        alarmsListView = view.findViewById(R.id.alarmsListView);
+        alarmAdapter = new MyAdapter(getContext(), alarmList);
+        alarmsListView.setAdapter(alarmAdapter);
+
+        //fetchAlarms(); // You'll implement this method to fetch alarms from ThingsBoard
     }
 
     @Override
