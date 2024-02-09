adcd5f4b84be5203d1006319364780ed2e799629
negiinj <neginjahan10@gmail.com>
2024-02-09 16:17:38 +0100

    ALARMS CHECK

--- app/src/main/java/com/example/hydroponic_farm/ui/notifications/NotificationsFragment.java
+++ app/src/main/java/com/example/hydroponic_farm/ui/notifications/NotificationsFragment.java
@@ -13,6 +13,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.hydroponic_farm.MyAdapter;
+import com.example.hydroponic_farm.ui.notifications.Alarm;
 import com.example.hydroponic_farm.R;
 import com.example.hydroponic_farm.databinding.FragmentNotificationsBinding;
 
@@ -25,6 +26,8 @@ public class NotificationsFragment extends Fragment {
 
     private ListView alarmsListView;
     private MyAdapter alarmAdapter;
+    SharedPreferences sharedPref;
+    String token;
     private ArrayList<Alarm> alarmList = new ArrayList<>();
 
     public NotificationsFragment() {
@@ -42,10 +45,15 @@ public class NotificationsFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        sharedPref = requireContext().getSharedPreferences("DIRECTORY1", Context.MODE_PRIVATE);
+        token = sharedPref.getString("token", "");
+        dataset = new AlarmsDataset(getContext(),token);
+        dataset.fill();
 
         alarmsListView = view.findViewById(R.id.alarmsListView);
-        alarmAdapter = new MyAdapter(getContext(), alarmList);
-        alarmsListView.setAdapter(alarmAdapter);
+        MyAdapter a = new MyAdapter(getContext(), alarmList);
+        alarmsListView.setAdapter(a);
+        a.notifyDataSetChanged();
 
         //fetchAlarms(); // You'll implement this method to fetch alarms from ThingsBoard
     }
